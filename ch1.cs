6d00f45cd93b05d80669417ba4bd8aa7c8bcea17
Sergey <61425252+vartabrox@users.noreply.github.com>
2022-06-14 15:50:05 +0300

    Delete ch1.cs

--- ch1.cs
+++
@@ -1,9 +0,0 @@
-class MyBestApp
-{
- static void Main() // The Main is the entry point of the program
- {
- System.Console.WriteLine("Hello NextNode"); // Using the method WriteLine, i was able to output the string "Hello NextNode"
- System.Console.WriteLine("Next " + "Node");
- }
-}
-// I also tested the comments
